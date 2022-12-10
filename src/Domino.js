b783720be7fe91f58c9337792d1ed432ca82575b
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2022-12-09 18:45:47 -0600

    Domino styles and components added

--- src/Domino.js
+++ src/Domino.js
@@ -1,19 +1,27 @@
 import './Domino.scss';
 
 import TopHeader from './components/TopHeader/TopHeader';
+import Promotions from './components/Promotions/Promotions';
+import Services from './components/Services/Services';
+import Installations from './components/Installations/Installations';
+import Gallery from './components/Gallery/Gallery';
+import VideoSection from './components/VideoSection/VideoSection';
+import Schedules from './components/Schedules/Schedules';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="domino-app">
 
       <div className="main-container">
-
-          {/* Header */}
           <TopHeader/>
-          <TopHeader/>
-          <TopHeader/>
-
-
+          <Promotions/>
+          <Services/>
+          <Installations/>
+          <Gallery/>
+          <VideoSection/>
+          <Schedules/>
+          <Footer/>
       </div>
 
     </div>
