886af3b96838fe9836f281c9c3a502ad4c9feb5b
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2022-12-11 17:10:01 -0600

    utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,7 @@
 export function Subtitle(props) {
     return <h2 className="size-30">~ { props.title } ~</h2>
 }
+
+export function Button(props) {
+    return <div className="domino-button text-center text-uppercase">{ props.title }</div>
+}
