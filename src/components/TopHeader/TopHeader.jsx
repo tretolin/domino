54110c882898e695117d38671f0e9bff91919c43
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2022-12-11 17:09:02 -0600

    Top Header component

--- src/components/TopHeader/TopHeader.jsx
+++ src/components/TopHeader/TopHeader.jsx
@@ -2,15 +2,43 @@ import "./TopHeader.scss";
 import React from "react";
 
 import BrandLogo from '../../images/BrandLogo'
+import {IconLocation, IconWhatsapp, IconPhone } from '../../images/Icons'
+import { Button } from '../../utils/utils';
+import config from '../../utils/config'
 
 const TopHeader = () => {
   return (
     <section id="top-header">
-      <div>
+      {/* Location button */}
+        <div className="header-icon">
+          <a href={config.location} target="_blank" rel="noopener noreferrer"> 
+            <IconLocation/>
+          </a>
+        </div>
 
-      <BrandLogo />
+      {/* Main brand logo */}
+      <div className="main-logo">
+        <div>
+          <BrandLogo/>
+        </div>
+        <div className="mt-4">
+          <a href={config.booking} target="_blank" rel="noopener noreferrer"> 
+            <Button title="Agendar cita"></Button>
+          </a>
+        </div>
+        
+        <div className="mt-4">
+            <div><IconPhone/> {config.phoneNumber}</div>
+        </div>
 
       </div>
+
+      {/* Whatsapp button */}
+      <div className="header-icon big">
+          <a href={config.whatsapp} target="_blank" rel="noopener noreferrer"> 
+            <IconWhatsapp/>
+          </a>
+        </div>
     </section>
   );
 }
