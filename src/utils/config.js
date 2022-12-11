7d7902a187d26cfebcec5d4d3510c2cb76ec4d86
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2022-12-11 17:09:41 -0600

    Config file

---
+++ src/utils/config.js
@@ -0,0 +1,8 @@
+const config = {
+    whatsapp: 'https://wa.me/message/LS4X623YJDEAF1',
+    location: 'https://www.google.com/maps/place/Domin%C3%B3+Barberia+Tradicional/@25.7640535,-100.2953091,17z/data=!3m1!4b1!4m5!3m4!1s0x86629535098dd0f9:0x6b3a6b7598ba76c!8m2!3d25.7640535!4d-100.2931204',
+    phoneNumber:'8131346173',
+    booking:'https://domin32.setmore.com/'
+}
+
+export default config;
