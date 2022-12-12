864877c05d2c867eb220906e44a2640b2bcfec76
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2022-12-11 18:22:45 -0600

    Promotions slide

--- src/components/Promotions/Promotions.jsx
+++ src/components/Promotions/Promotions.jsx
@@ -1,11 +1,24 @@
-import "./Promotions.css";
+import "./Promotions.scss";
 import React from "react";
-import { Subtitle } from "../../utils/utils";
+// import { Subtitle } from "../../utils/utils";
 
 const Promotions = () => {
   return (
     <section id="promotions" className="text-center">
-      <Subtitle title="promociones"/>
+      {/* <Subtitle title="promociones"/> */}
+      <div className="promo-slider">
+        <div className="promo-container">
+          {/* <div>A</div>
+          <div>B</div>
+          <div>C</div> */}
+          <div className="slide-group">
+            <img src="/images/promo-01.jpg" alt="promo" width="100%" />
+            <img src="/images/promo-02.jpg" alt="promo" width="100%" />
+            <img src="/images/promo-03.jpg" alt="promo" width="100%" />
+          </div>
+
+        </div>
+      </div>
     </section>
   );
 }
