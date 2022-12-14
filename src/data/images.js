0fd80c268cdd1c52c6f3809a92e1e45a7b5a7915
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2022-12-14 17:06:14 -0600

    Images

---
+++ src/data/images.js
@@ -0,0 +1,18 @@
+export const installations = [
+    'installations/ins-01.jpg',
+    'installations/ins-02.jpg',
+    'installations/ins-03.jpg',
+    'installations/ins-04.jpg',
+    'installations/ins-05.jpg',
+    'installations/ins-06.jpg',
+    'installations/ins-07.jpg',
+]
+
+export const gallery = [
+    'gallery/g-1.jpg',
+    'gallery/g-2.jpg',
+    'gallery/g-3.jpg',
+    'gallery/g-4.jpg',
+    'gallery/g-5.jpg',
+    'gallery/g-6.jpg',
+]
