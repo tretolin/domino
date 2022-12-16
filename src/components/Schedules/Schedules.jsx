206cf7eeab0863e5744dfb13d16006d4d2c7b505
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2022-12-15 20:22:25 -0600

    Schedules section

--- src/components/Schedules/Schedules.jsx
+++ src/components/Schedules/Schedules.jsx
@@ -1,11 +1,21 @@
-import "./Schedules.css";
+import "./Schedules.scss";
 import React from "react";
 import { Subtitle } from "../../utils/utils";
+
+import { schedules } from '../../data/schedules.js';
 
 const Schedules = () => {
   return (
     <section id="schedules" className="text-center">
       <Subtitle title="Horarios"/>
+      <div className="schedules">
+        { schedules.map( (schedule, index) => 
+            <div className="schedule-info" key={index}>
+              <div className="week-day">{ schedule.day }</div>
+              <div className="schedule">{ schedule.start } - { schedule.end }</div>
+            </div>
+          ) }
+      </div>
     </section>
   );
 }
--- src/components/Schedules/index.js
+++
@@ -1,2 +0,0 @@
-import Schedules from "./Schedules";
-export default Schedules;
