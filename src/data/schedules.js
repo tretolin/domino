3d0bd9b0d746444f657822d8c4c9784497a6db81
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2022-12-15 20:23:10 -0600

    Added schedules to data

---
+++ src/data/schedules.js
@@ -0,0 +1,18 @@
+export const schedules = [
+    {
+        day: 'Martes a Viernes',
+        start: '11 AM',
+        end: ' 8 PM'
+    },
+    
+    {
+        day: 'Sábado',
+        start: '9 AM',
+        end: ' 5 PM'
+    },
+    {
+        day: 'Domingo',
+        start: '11 AM',
+        end: '5 PM'
+    },
+]
