544fa90df90615c22120591f998eddbadd3b0f7e
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2022-12-16 18:26:41 -0600

    Footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,48 @@
-import "./Footer.css";
+import "./Footer.scss";
 import React from "react";
-import { Subtitle } from "../../utils/utils";
+import {IconLocation, IconWhatsapp, IconInstagram} from '../../images/Icons'
+import VariantLogo from '../../images/VariantLogo'
+
+const contactData = [
+  {
+    title: "Plaza Andenes local A-211",
+    description: "Avenida Universidad 1250-nte, Col. Los Villarreal, 66447 San Nicolás de los Garza, N.L.",
+    icon: 'location'
+  },
+  {
+    title: "Whatsapp",
+    description: "8131346173",
+    icon: 'whatsapp'
+  },
+  {
+    title: "barberiadomino",
+    description: "",
+    icon: 'instagram'
+  },
+]
 
 const Footer = () => {
   return (
     <section id="footer" className="text-center">
-      <Subtitle title="Footer"/>
+      <div>
+        {
+          contactData.map( (item, index) => 
+            <div className="contact-item">
+              <div className="icon">
+                <div>
+                  { [<IconLocation/>,<IconWhatsapp/>,<IconInstagram/>][index] }
+                </div>
+              </div>
+              <div>
+                <div className="title">{ item.title }</div>
+                <div>{ item.description }</div>
+              </div>
+            </div>
+          )
+        }
+
+      </div>
+      <div><VariantLogo/></div>
     </section>
   );
 }
