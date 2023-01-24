f8164ef8a2e8a71a77d1c3a4e0686ddd7dc0501c
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2023-01-23 22:33:35 -0600

    New schedules and responsive design

--- src/data/schedules.js
+++ src/data/schedules.js
@@ -1,18 +1,22 @@
 export const schedules = [
     {
-        day: 'Martes a Viernes',
+        day: 'Lunes',
         start: '11 AM',
         end: ' 8 PM'
     },
-    
+    {
+        day: 'Martes a Viernes',
+        start: '11 AM',
+        end: ' 9 PM'
+    },
     {
         day: 'Sábado',
-        start: '9 AM',
-        end: ' 5 PM'
+        start: '10 AM',
+        end: ' 8 PM'
     },
     {
         day: 'Domingo',
         start: '11 AM',
-        end: '5 PM'
+        end: '6 PM'
     },
 ]
